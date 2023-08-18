4f99f4698aec6dae0c1c55ec3d02bb2ec4cc3001
vicblanc92 <91130270+vicblanc92@users.noreply.github.com>
2023-08-18 18:15:32 +0200

    add config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: { inter: ['Inter'] },
+
+    minWidth: {
+      104: '104px',
+    },
+
+    colors: {
+      grey: {
+        50: '#E0E0E0',
+        100: '#838383',
+        200: '#575757',
+      },
+      blue: '#4D7AE3',
+    },
+    extend: {
+      width: {
+        374: '374px',
+        418: '418px',
+      },
+      height: { 136: '136px', 108: '108px', 156: '156px' },
+    },
   },
   plugins: [],
 };
